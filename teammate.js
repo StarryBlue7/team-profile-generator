7101e95c8eb2e8f5172ead5d86c9b89c8e6e03c0
Vince <starryblue7@gmail.com>
2021-10-16 14:16:54 -0700

    write teammate.js script to pass test

---
+++ teammate.js
@@ -0,0 +1,10 @@
+// Create a teammate object
+class Teammate {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+}
+
+module.exports = Teammate;
