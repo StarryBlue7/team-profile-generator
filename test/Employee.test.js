2ffc53167e159055767a2af37aa93088c8aebece
Vince <starryblue7@gmail.com>
2021-10-18 16:44:09 -0700

    add Employee object method tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,13 +1,25 @@
 const Employee = require('../Employee');
 
 describe('Employee', () => {
-    it('should create an object with a name, id, and email', () => {
+    it('should create an object with a name, id, email, and role of Employee', () => {
         const employee = new Employee('Ardbert', 1, 'WoD@ffxiv.com');
 
-        expect(employee).toEqual({name: 'Ardbert', id: 1, email: 'WoD@ffxiv.com'});
+        expect(employee).toEqual({name: 'Ardbert', id: 1, email: 'WoD@ffxiv.com', role: 'Employee'});
     });
 
-    describe('getName', () => {
-        it('should return the name value')
-    })
+    describe('get methods', () => {
+        it('should return the name value', () => {
+            const employee = new Employee('Ardbert', 1, 'WoD@ffxiv.com');
+
+            const name = employee.getName();
+            const id = employee.getId();
+            const email = employee.getEmail();
+            const role = employee.getRole();
+
+            expect(name).toBe('Ardbert');
+            expect(id).toBe(1);
+            expect(email).toBe('WoD@ffxiv.com');
+            expect(role).toBe('Employee');
+        });
+    });
 });
