6b81845460288809270c6b57fa3282a388a6f6a6
Vince <starryblue7@gmail.com>
2021-10-18 18:16:02 -0700

    refactor tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
-const { Manager, Engineer, Intern } = require('../Role');
+const Manager = require('../Manager');
+const Engineer = require('../Engineer');
+const Intern = require('../Intern');
 const index = require('../index');
 const pageGenerator = require('../pageGenerator');
 
@@ -8,18 +10,18 @@ const pageGenerator = require('../pageGenerator');
 describe('Team builder', () => {
 
     describe.each([
-        { role: 'manager', obj: {name: 'Exarch', id: 1, email: 'crystal@tower.gov', office: 12}, type: Manager },
-        { role: 'engineer', obj: {name: 'Yshtola', id: 2, email: 'yshtola@scions.org', gitHub: 'Matoya'}, type: Engineer },
-        { role: 'intern', obj: {name: 'Ryne', id: 3, email: 'ryne@first.org', school: 'Eulmore'}, type: Intern }
-    ])('addTeammate function', ({role, obj, type}) => {
+        { role: 'manager', answers: {name: 'Exarch', id: 1, email: 'crystal@tower.gov', office: 12}, object: new Manager('Exarch', 1, 'crystal@tower.gov', 12), type: Manager },
+        { role: 'engineer', answers: {name: 'Yshtola', id: 2, email: 'yshtola@scions.org', gitHub: 'Matoya'}, object: new Engineer('Yshtola', 2, 'yshtola@scions.org', 'Matoya'), type: Engineer },
+        { role: 'intern', answers: {name: 'Ryne', id: 3, email: 'ryne@first.org', school: 'Eulmore'}, object: new Intern('Ryne', 3, 'ryne@first.org', 'Eulmore'), type: Intern }
+    ])('addTeammate function', ({role, answers, object, type}) => {
         it('should add a teammate to team object in new role array if it does not already exist', () => {
             const team = {};
-        
-            const teammate = index.addTeammate(team, role, obj);
 
-            expect(teammate).toEqual(obj);
+            const teammate = index.addTeammate(team, role, answers);
+
+            expect(teammate).toEqual(object);
             expect(teammate).toBeInstanceOf(type);
-            expect(team[role]).toEqual([obj]);
+            expect(team[role]).toEqual([object]);
         });
         
         it('should push new teammates to existing role arrays', () => {
@@ -31,18 +33,18 @@ describe('Team builder', () => {
                 intern: [firstTeammate]
             };
         
-            const teammate = index.addTeammate(team, role, obj);
+            const teammate = index.addTeammate(team, role, answers);
 
-            expect(teammate).toEqual(obj);
+            expect(teammate).toEqual(object);
             expect(teammate).toBeInstanceOf(type);
-            expect(team[role]).toEqual([firstTeammate, obj]);
+            expect(team[role]).toEqual([firstTeammate, object]);
         });
     });
 
     describe.each([
         {role: 'engineer'},
         {role: 'intern'}
-    ])('nextQuery function', (role) => {
+    ])('nextQuery function', () => {
         it('should generate the HTML page when Finished adding is chosen', () => {
             const team = {team: 'set'};
             const answers = {next: 'Finished adding'};
